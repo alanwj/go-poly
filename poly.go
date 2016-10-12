177e018e3625e9efae9444fee56f27711cfe6e26
Alan Johnson <alan.wayne.johnson@gmail.com>
2016-10-12 01:27:08 -0700

    Add a polynomial class and tests.

---
+++ poly.go
@@ -0,0 +1,190 @@
+// The poly package provides types and functions for manipulating polynomials.
+package poly
+
+import (
+	"bytes"
+	"fmt"
+	"math"
+)
+
+// Poly represents a polynomial of arbitrary degree.
+// A zero valued Poly is equivalent to 0.0.
+type Poly struct {
+	coeff []float64
+}
+
+// Returns the coefficient array for a Poly.
+// This level of indirection makes implementing the zero valued Poly easier.
+func (p Poly) co() []float64 {
+	if len(p.coeff) == 0 {
+		return []float64{0}
+	}
+	return p.coeff
+}
+
+// Returns a normalized polynomial with the given coefficients.
+// All leading terms with degree greater than 0 and coefficients that are zero
+// within the precision of a float64 are removed.
+func normalized(c []float64) Poly {
+	i := len(c) - 1
+	for i > 0 && c[i] == 0.0 {
+		i--
+	}
+	return Poly{c[0 : i+1]}
+}
+
+// Creates a new Poly.
+// The ith parameter represents the coefficient of x^i.
+// Example:
+//   p := poly.New(1.5, 2.3, 3.7)
+//
+//   This represents 1.5 + 2.3*x + 3.7*x^2
+func New(c ...float64) Poly {
+	if len(c) == 0 {
+		return Poly{[]float64{0.0}}
+	}
+	a := make([]float64, len(c))
+	copy(a, c)
+	return normalized(a)
+}
+
+// Returns the highest degree of the polynomial's highest order term.
+func (p Poly) Deg() int {
+	return len(p.co()) - 1
+}
+
+// Returns the coefficient of the ith order term.
+func (p Poly) Coeff(i int) float64 {
+	if i < 0 || i > p.Deg() {
+		return 0.0
+	}
+	return p.co()[i]
+}
+
+// Evaluates a polynomial at the given point x.
+func (p Poly) Eval(x float64) float64 {
+	var n float64
+	for i, c := range p.co() {
+		n += c * math.Pow(x, float64(i))
+	}
+	return n
+}
+
+// Adds a polynomial to another polynomial.
+// Returns p+q.
+func (p Poly) Add(q Poly) Poly {
+	pco := p.co()
+	plen := len(pco)
+	qco := q.co()
+	qlen := len(qco)
+	if plen < qlen {
+		return q.Add(p)
+	}
+
+	c := make([]float64, plen)
+
+	pco = p.co()
+	for i, qc := range qco {
+		c[i] = pco[i] + qc
+	}
+
+	for i := qlen; i < plen; i++ {
+		c[i] = pco[i]
+	}
+
+	return normalized(c)
+}
+
+// Subtracts a polynomial from another polynomial.
+// Returns p-q.
+func (p Poly) Sub(q Poly) Poly {
+	qco := q.co()
+	qlen := len(qco)
+	c := make([]float64, qlen)
+	for i, qc := range qco {
+		c[i] = -qc
+	}
+	return p.Add(Poly{c})
+}
+
+// Multiplies a polynomial by another polynomial.
+// Returns p*q.
+func (p Poly) Mul(q Poly) Poly {
+	pco := p.co()
+	plen := len(pco)
+	qco := q.co()
+	qlen := len(qco)
+	c := make([]float64, plen+qlen-1)
+	for i, pc := range pco {
+		for j, qc := range qco {
+			c[i+j] += pc * qc
+		}
+	}
+	return normalized(c)
+}
+
+// Computes the derivative of a polynomial.
+func (p Poly) Der() Poly {
+	pco := p.co()
+	plen := len(pco)
+	c := make([]float64, plen-1)
+	for i, pc := range pco {
+		if i > 0 {
+			c[i-1] = pc * float64(i)
+		}
+	}
+	return normalized(c)
+}
+
+// Computes the definite integral of a polynomial.
+// The provided constant k will be used as the 0th order term of the result.
+func (p Poly) Int(k float64) Poly {
+	pco := p.co()
+	plen := len(pco)
+	c := make([]float64, plen+1)
+	c[0] = k
+	for i, pc := range pco {
+		c[i+1] = pc / float64(i+1)
+	}
+	return normalized(c)
+}
+
+// Returns a printable string representing the polynomial value.
+func (p Poly) String() string {
+	var buffer bytes.Buffer
+
+	pco := p.co()
+	plen := len(pco)
+
+	first := true
+	for i := plen; i > 0; i-- {
+		e := i - 1
+		absc := math.Abs(pco[e])
+		if absc < 0.0001 && !(first && e == 0) {
+			continue
+		}
+
+		c := pco[e]
+		if !first {
+			if c < 0 {
+				buffer.WriteString(" - ")
+			} else {
+				buffer.WriteString(" + ")
+			}
+			c = absc
+		}
+		if absc != 1.0 || e == 0 {
+			buffer.WriteString(fmt.Sprintf("%.3f", c))
+		} else if c == -1.0 && first {
+			buffer.WriteString("-")
+		}
+		if e != 0 {
+			buffer.WriteString("x")
+			if e != 1 {
+				buffer.WriteString(fmt.Sprintf("^%d", e))
+			}
+		}
+		first = false
+	}
+	return buffer.String()
+}
---
+++ poly_test.go
@@ -0,0 +1,247 @@
+package poly
+
+import (
+	"math"
+	"testing"
+)
+
+func comparePoly(p, q Poly) bool {
+	if p.Deg() != q.Deg() {
+		return false
+	}
+	for i := 0; i <= p.Deg(); i++ {
+		if math.Abs(p.Coeff(i)-q.Coeff(i)) > 0.00001 {
+			return false
+		}
+	}
+	return true
+}
+
+// Tests that the degree of various polynomials is reported as expected.
+func TestDeg(t *testing.T) {
+	cases := []struct {
+		p    Poly
+		want int
+	}{
+		{New(), 0},
+		{New(1), 0},
+		{New(1, 2), 1},
+		{New(1, 2, 3), 2},
+		{New(0, 0, 0), 0},
+	}
+	for i, c := range cases {
+		if got := c.p.Deg(); got != c.want {
+			t.Errorf("case %d: Deg() on %q == %d, want %d", i, c.p, got, c.want)
+		}
+	}
+}
+
+// Tests the degree of a zero valued Poly.
+func TestDegZero(t *testing.T) {
+	var p Poly
+	want := 0
+	if got := p.Deg(); got != want {
+		t.Errorf("Deg() == %d, want %d", got, want)
+	}
+}
+
+// Tests that the coefficients of various terms are correctly reported.
+func TestCoeff(t *testing.T) {
+	c := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	p := New(c...)
+	for i, want := range c {
+		if got := p.Coeff(i); got != want {
+			t.Errorf("Coeff(%d) == %f, want %f", i, got, want)
+		}
+	}
+}
+
+// Tests the coefficient of a zero valued Poly.
+func TestCoeffZero(t *testing.T) {
+	var p Poly
+	want := float64(0)
+	if got := p.Coeff(0); got != want {
+		t.Errorf("Coeff(0) == %f, want %f", got, want)
+	}
+}
+
+// Tests that out of range coefficients are zero.
+func TestCoeffOutOfRange(t *testing.T) {
+	cases := []struct {
+		i    int
+		want float64
+	}{
+		{-2, 0.0},
+		{-1, 0.0},
+		{0, 1.0},
+		{1, 2.0},
+		{2, 3.0},
+		{3, 0.0},
+		{4, 0.0},
+	}
+	p := New(1, 2, 3)
+	for i, c := range cases {
+		if got := p.Coeff(c.i); got != c.want {
+			t.Errorf("case %d: Coeff(%d) == %f, want %f", i, c.i, got, c.want)
+		}
+	}
+}
+
+// Tests that function evaluation produces correct results.
+func TestEval(t *testing.T) {
+	cases := []struct {
+		p    Poly
+		x    float64
+		want float64
+	}{
+		{Poly{}, 0.0, 0.0},
+		{New(), 0.0, 0.0},
+		{New(0, 1, 2), 0.0, 0.0},
+		{New(1, 2, 3), 0.0, 1.0},
+		{New(-1, 2, -3), 2.5, -14.75},
+	}
+	for i, c := range cases {
+		if got := c.p.Eval(c.x); math.Abs(c.want-got) > 0.00001 {
+			t.Errorf("case %d: Eval(%f) on %q == %f, want %f", i, c.x, c.p, got, c.want)
+		}
+	}
+}
+
+// Tests that polynomials add correctly.
+func TestAdd(t *testing.T) {
+	cases := []struct {
+		p    Poly
+		q    Poly
+		want Poly
+	}{
+		{Poly{}, Poly{}, Poly{}},
+		{New(), New(), New()},
+		{New(1, 2), Poly{}, New(1, 2)},
+		{Poly{}, New(1, 2), New(1, 2)},
+		{New(1, 2), New(3, 4), New(4, 6)},
+		{New(1, 2, 3), New(3, 4), New(4, 6, 3)},
+		{New(1, 2), New(3, 4, 5), New(4, 6, 5)},
+		{New(1, 2, 3), New(-1, 2, -3), New(0, 4)},
+	}
+	for i, c := range cases {
+		if got := c.p.Add(c.q); !comparePoly(got, c.want) {
+			t.Errorf("case %d: Add(%q) on %q == %q, want %q", i, c.q, c.p, got, c.want)
+		}
+	}
+}
+
+// Tests that polynomials subtract correctly.
+func TestSub(t *testing.T) {
+	cases := []struct {
+		p    Poly
+		q    Poly
+		want Poly
+	}{
+		{Poly{}, Poly{}, Poly{}},
+		{New(), New(), New()},
+		{New(1, 2), Poly{}, New(1, 2)},
+		{Poly{}, New(1, 2), New(-1, -2)},
+		{New(1, 2), New(3, 4), New(-2, -2)},
+		{New(1, 2, 3), New(3, 4), New(-2, -2, 3)},
+		{New(1, 2), New(3, 4, 5), New(-2, -2, -5)},
+		{New(1, 4, 3), New(1, 2, 3), New(0, 2)},
+	}
+	for i, c := range cases {
+		if got := c.p.Sub(c.q); !comparePoly(got, c.want) {
+			t.Errorf("case %d: Sub(%q) on %q == %q, want %q", i, c.q, c.p, got, c.want)
+		}
+	}
+}
+
+// Tests that polynomials multiply correctly.
+func TestMul(t *testing.T) {
+	cases := []struct {
+		p    Poly
+		q    Poly
+		want Poly
+	}{
+		{Poly{}, Poly{}, Poly{}},
+		{New(), New(), New()},
+		{New(1, 2), Poly{}, Poly{}},
+		{Poly{}, New(1, 2), Poly{}},
+		{New(2, 1), New(-2, 1), New(-4, 0, 1)},
+		{New(1, 2), New(3, 4), New(3, 10, 8)},
+		{New(1, 2, 3), New(3, 4), New(3, 10, 17, 12)},
+		{New(3, 4), New(1, 2, 3), New(3, 10, 17, 12)},
+	}
+	for i, c := range cases {
+		if got := c.p.Mul(c.q); !comparePoly(got, c.want) {
+			t.Errorf("case %d: Mul(%q) on %q == %q, want %q", i, c.q, c.p, got, c.want)
+		}
+	}
+}
+
+// Tests that derivatives are computed correctly.
+func TestDer(t *testing.T) {
+	cases := []struct {
+		p    Poly
+		want Poly
+	}{
+		{Poly{}, Poly{}},
+		{New(), New()},
+		{New(1), New(0)},
+		{New(1, 2), New(2)},
+		{New(1, 2, 3), New(2, 6)},
+	}
+	for i, c := range cases {
+		if got := c.p.Der(); !comparePoly(got, c.want) {
+			t.Errorf("case %d: Der() on %q == %q, want %q", i, c.p, got, c.want)
+		}
+	}
+}
+
+// Tests that integrals are computed correctly.
+func TestInt(t *testing.T) {
+	cases := []struct {
+		p    Poly
+		k    float64
+		want Poly
+	}{
+		{Poly{}, 0, Poly{}},
+		{New(), 0, New()},
+		{Poly{}, 3, New(3)},
+		{New(1), 4, New(4, 1)},
+		{New(1, 4), 5, New(5, 1, 2)},
+	}
+	for i, c := range cases {
+		if got := c.p.Int(c.k); !comparePoly(got, c.want) {
+			t.Errorf("case %d: Int(%.3f) on %q == %q, want %q", i, c.k, c.p, got, c.want)
+		}
+	}
+}
+
+// Tests that the string representation is correct.
+func TestString(t *testing.T) {
+	cases := []struct {
+		p    Poly
+		want string
+	}{
+		{Poly{}, "0.000"},
+		{New(), "0.000"},
+		{New(1.234), "1.234"},
+		{New(-1.234), "-1.234"},
+		{New(0, 1), "x"},
+		{New(0, -1), "-x"},
+		{New(0, 2), "2.000x"},
+		{New(0, -2), "-2.000x"},
+		{New(0, 0, 1), "x^2"},
+		{New(0, 0, -1), "-x^2"},
+		{New(0, 0, 2), "2.000x^2"},
+		{New(0, 0, -2), "-2.000x^2"},
+		{New(0, 1, 1), "x^2 + x"},
+		{New(0, 2, 2), "2.000x^2 + 2.000x"},
+		{New(0, -1, -1), "-x^2 - x"},
+		{New(0, -2, -2), "-2.000x^2 - 2.000x"},
+		{New(-3, -1, 2, 0, 4), "4.000x^4 + 2.000x^2 - x - 3.000"},
+	}
+	for i, c := range cases {
+		if got := c.p.String(); got != c.want {
+			t.Errorf("case %d: String() on %q == %q, want %q", i, c.p, got, c.want)
+		}
+	}
+}
